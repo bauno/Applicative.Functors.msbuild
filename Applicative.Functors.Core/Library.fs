482947e642a436ef01de7d4aad7284e7acdc23dc
Alessandro Bonometti <a.bonometti@gmail.com>
2022-01-01 15:38:10 +0100

    Plumbing and fake tests

---
+++ Applicative.Functors.Core/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Applicative.Functors.Core
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ Applicative.Functors.Tests/Core.Tests.fs
@@ -0,0 +1,12 @@
+module Applicative.Functors.Tests.Core
+
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``I can pass the first dumb test`` () =
+    true |> should be True
+
+[<Fact>]
+let ``I can pass the second dumb test`` () =
+    1 + 1 |> should equal 2 
